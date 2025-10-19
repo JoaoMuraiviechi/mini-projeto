edf460874fe8dee13c1ef676f834d29c154ec914
JoaoMuraiviechi <joao.muraiviechi@gmail.com>
2025-10-19 16:35:28 -0300

    fix: corrigindo como aparece no vercel

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { register, login, protectedRoute } from "../controllers/authController";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import { health } from "../controllers/healthController ";
 
 const router = Router();
 
+router.get("/", health); // rota raiz
 router.post("/register", register);
 router.post("/login", login);
 router.get("/protected", authMiddleware, protectedRoute);
