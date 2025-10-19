aaa4246b7716df5861ec60c242c7a962b1c50447
JoaoMuraiviechi <joao.muraiviechi@gmail.com>
2025-10-19 16:37:44 -0300

    fix: atualizando o npm

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { register, login, protectedRoute } from "../controllers/authController";
 import { authMiddleware } from "../middlewares/authMiddleware";
-import { health } from "../controllers/healthController ";
+import { health } from "../controllers/healthController";
 
 const router = Router();
 
