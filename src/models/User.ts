c93c69073f37161c667175c46b4819f336092a2d
JoaoMuraiviechi <joao.muraiviechi@gmail.com>
2025-10-19 13:21:15 -0300

    fix: corrigindo erros do userService

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,10 +6,13 @@ export interface IUser extends Document {
   password: string;
 }
 
-const userSchema = new Schema<IUser>({
-  name: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-  password: { type: String, required: true, select: false }
-}, { timestamps: true });
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, select: false },
+  },
+  { timestamps: true }
+);
 
-export const User = mongoose.model<IUser>("User", userSchema);
+export const User = mongoose.model<IUser>("User", UserSchema);
