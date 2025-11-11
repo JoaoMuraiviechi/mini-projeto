32d217f27ac3319039882611d713b69d535da7f4
JoaoMuraiviechi <joao.muraiviechi@gmail.com>
2025-11-11 18:07:08 -0300

    .

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://mini-projeto-cl4a.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
